767d5f937b695d235143cd5ec0a5a3639ad9cbfe
Joel Harris <joelharris@Joels-Air.attlocal.net>
2015-12-01 23:45:50 -0600

    added all basic dependencies, grunt working

--- app/app.js
+++ app/app.js
@@ -1,13 +1,14 @@
-console.log("index accessing app.js")
 OAuth.initialize('j_EDxkRRXclXp8B6XZC4TnJUoB4');
 //Using popup
-OAuth.popup('fitbit')
-    .done(function(result) {
+function loginFitbit(){
+	OAuth.popup('fitbit')
+	   .done(function(result) {
 
-    	console.log(result);
+		console.log(result);
       //use result.access_token in your API request 
       //or use result.get|post|put|del|patch|me methods (see below)
-    })
-    .fail(function (err) {
-      //handle error with err
-});
+	    })
+	   .fail(function (err) {
+	   //handle error with err
+	});
+}
