6d2d1b2bac789c19f66e4eb3e4bfb54f9534d772
Joel Harris <joelharris@Joels-Air.attlocal.net>
2015-12-07 23:15:59 -0600

    generating random numbers for both start and stops

--- app/controllers/singleconcert.js
+++ app/controllers/singleconcert.js
@@ -5,14 +5,24 @@ app.controller('concertCtrl',
 	'$location',
 	function($firebaseObject,fireAuth,$routeParams,$location){
 
-		this.selectedSong = {};
-
+		//creates reference to particular concert
 		this.Id= $routeParams.id;
 		console.log(this.Id);
 		var ref = new Firebase("https://boogie.firebaseio.com/concerts/"+this.Id);
 			
+		//concert object
 		this.concert = $firebaseObject(ref);
 		console.log(this.concerts);
+
+		//gets initial steps before concert
+		this.startCount = function(){
+			var initialSteps = (Math.floor(Math.random()*50)+1)*100;
+		}
+
+		//gets steps after concert
+		this.stopCount = function(){
+			var postSteps = (Math.floor(Math.random()*50)+51)*100;
+		}
 
 	}
 ])
