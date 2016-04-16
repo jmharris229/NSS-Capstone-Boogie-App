2b6857d4701df7c2dabb04421222daaab5af0f29
Joel Harris <joelharris229@gmail.com>
2016-04-16 16:41:09 -0500

    fixed api request for BIT and changed page title

--- app/services/searchReq.js
+++ app/services/searchReq.js
@@ -3,7 +3,7 @@ app.service("bitreq", function ($http, $q) {
 
     //creates pre generated api call with phish as criteria
     this.getResults = function () {
-        return $http.jsonp('http://api.bandsintown.com/artists/Phish/events.json?api_version=2.0&app_id=YOUR_APP_ID'+ '&callback=JSON_CALLBACK')
+        return $http.jsonp('//api.bandsintown.com/artists/Phish/events.json?api_version=2.0&app_id=YOUR_APP_ID'+ '&callback=JSON_CALLBACK')
             .then(function (response) {
                 // promise is fulfilled
                 deferred.resolve(response.data);
@@ -20,7 +20,7 @@ app.service("bitreq", function ($http, $q) {
     //makes api call with user search critieria
     this.getResultsSearch = function(bandname){
         var deferred = $q.defer();
-        return $http.jsonp('http://api.bandsintown.com/artists/'+bandname+'/events.json?api_version=2.0&app_id=YOUR_APP_ID'+ '&callback=JSON_CALLBACK')
+        return $http.jsonp('//api.bandsintown.com/artists/'+bandname+'/events.json?api_version=2.0&app_id=YOUR_APP_ID'+ '&callback=JSON_CALLBACK')
             .then(function (response) {
                 // promise is fulfilled
                 deferred.resolve(response.data);
